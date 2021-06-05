259fe75674d4190c589cb8e9d1667bffeef1b059
EC2 Default User <ec2-user@ip-172-31-29-228.us-east-2.compute.internal>
2021-06-05 09:19:17 +0000

    Updated config

--- src/config.js
+++ src/config.js
@@ -2,16 +2,16 @@ export default {
   MAX_ATTACHMENT_SIZE: 5000000,
   s3: {
     REGION: "ap-south-1",
-    BUCKET: "sharebox-files-bucket"
+    BUCKET: "sharebox-app-upload"
   },
   apiGateway: {
     REGION: "ap-south-1",
-    URL: "https://ylizg2zq65.execute-api.ap-south-1.amazonaws.com/prod"
+    URL: "https://7megdztqje.execute-api.ap-south-1.amazonaws.com/prod"
   },
   cognito: {
     REGION: "ap-south-1",
-    USER_POOL_ID: "ap-south-1_htyoykdtT",
-    APP_CLIENT_ID: "6fc31nohsrojqrn6is314p93kd",
-    IDENTITY_POOL_ID: "ap-south-1:f0894763-0008-44c3-a9b5-36caa789caa4"
+    USER_POOL_ID: "ap-south-1_7xrzSFhsl",
+    APP_CLIENT_ID: "sdla6d7mtnvol60rg2ceblvea",
+    IDENTITY_POOL_ID: "ap-south-1:48644e8c-222e-48b3-ad49-70280310f815"
   }
 };
